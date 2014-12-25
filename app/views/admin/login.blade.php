a61a22ca73084b6ef35d7b19d85a9c6f021e072e
ManuLEM <manuel.lemaire@gmail.com>
2014-12-25 01:57:45 -0500

    login interface

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -1,9 +1,13 @@
 @section('content')
     <section id="login-section">
+        <h1>Connexion</h1>
         <form method="post" action="{{ URL::route('postLogin') }}" autocomplete="off">
-            <input type="text" name="username" placeholder="Username">
-            <input type="password" name="password" placeholder="Password">
-            <input type="submit" value="Login">
+            <input type="text" name="username" placeholder="Nom d'utilisateur">
+            <span class="blur"></span>
+            <input type="password" name="password" placeholder="Mot de passe">
+            <span class="blur"></span>
+            <button type="submit"><i class="fa fa-lock"></i>Connexion</button>
+            <span class="blur"></span>
         </form>
         @if($errors->has('credentials'))
             <p class="error">{{$errors->first('credentials')}}</p>
@@ -12,4 +16,17 @@
             <p class="error">{{$errors->first('expire')}}</p>
         @endif
     </section>
+    <script>
+        var blurs = document.querySelectorAll('span.blur');
+        for (var i = 0; i < blurs.length; i++) {
+            blurs[i].style.backgroundSize = window.innerWidth + "px auto";
+            if (i < 2) {
+                blurs[i].style.backgroundPositionY = "-" + (- 103 + blurs[i].offsetParent.offsetTop + blurs[i].offsetTop) + "px";
+            }
+            else {
+                blurs[i].style.backgroundPositionY = "-" + (- 100 + blurs[i].offsetParent.offsetTop + blurs[i].offsetTop) + "px";
+            }
+            blurs[i].style.backgroundPositionX = "-" + (blurs[i].offsetParent.offsetLeft + 3) + "px";
+        };
+    </script>
 @stop
--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Matt's guitars</title>
+    <!-- <link href='http://fonts.googleapis.com/css?family=Alegreya:400,700' rel='stylesheet' type='text/css'> -->
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 </head>
 <body>
     @yield('content')
